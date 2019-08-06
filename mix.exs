305523a6f47d7cde8342e06e23c498f635e9167c
Jesse Cooke <jesse@relativepath.io>
2019-08-06 15:37:31 -0700

    Add docs because hex

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule PlugTeapot.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug, "~> 1.8"}
+      {:plug, "~> 1.8"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
